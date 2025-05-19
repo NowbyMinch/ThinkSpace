77d6b9a9c9380d2fde125b3f983197126e38de1a
Monteiro <guilhermemr2011j3@gmail.com>
2025-05-19 16:09:25 -0300

    sei la cara nao aguento mais

--- app/materiais/[id]/page.tsx
+++ app/materiais/[id]/page.tsx
@@ -1,7 +1,4 @@
-import MateriaisClient from "./MateriaisClient";
-import {  CirclePlus, Heart, Globe, Monitor, CodeXml, HeartPulse, Minus, Divide, X, Plus, Search, ChevronRight, ChevronsLeft, ChevronsRight, ChevronLeft, ChevronDown, BookOpenText, FileText, FileInput, ScrollText } from "lucide-react";
-import {  useEffect, useState  } from 'react';
-
+import MateriaisClient from './MateriaisClient';
 
 export default function Materiais({ params }: {params: { id: string } }) {
     return <MateriaisClient id={params.id} />
