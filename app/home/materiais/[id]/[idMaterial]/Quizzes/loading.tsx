3ad1af0055cccb3131bfaaab6088a1d06cd9e831
Monteiro <guilhermemr2011j3@gmail.com>
2025-06-07 18:44:00 -0300

    update

---
+++ app/home/materiais/[id]/[idMaterial]/Quizzes/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <div className="animate-spin w-10 h-10 border-4 border-purple-500 border-t-transparent rounded-full" />
+    </div>
+  )
+}
