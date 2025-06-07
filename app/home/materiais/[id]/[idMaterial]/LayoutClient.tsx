228eae08fabb84f4d05fa4a7071cbc257c4d90e1
Monteiro <guilhermemr2011j3@gmail.com>
2025-06-07 18:40:26 -0300

    update

--- app/home/materiais/[id]/[idMaterial]/LayoutClient.tsx
+++ app/home/materiais/[id]/[idMaterial]/LayoutClient.tsx
@@ -2,54 +2,68 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation"
+import { Reply } from "lucide-react";
+import { motion } from "framer-motion";
 
 export default function LayoutClient({ id, idMaterial, }: { id: string; idMaterial: string;}) { 
     const pathname = usePathname();
     return (
         <>
-            <div className="flex flex-col w-full h-full relative overflow-hidden ">
-                <div className="flex gap-6">
-                    <Link href={`/home/materiais/${id}/${idMaterial}/Material`} className="flex items-center">
-                        {(() => {
-                            if (pathname == `/home/materiais/${id}/${idMaterial}/Material`) {
-                                return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center">Material</h2>
-                            }
-                            return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center text-[rgb(0,0,0,54%)]">Material</h2>
-                            }
-                        )()}
-                        
-                    </Link>    
-                    <Link href={`/home/materiais/${id}/${idMaterial}/Resumo`} className="flex items-center">
-                        {(() => {
-                            if (pathname == `/home/materiais/${id}/${idMaterial}/Resumo`) {
-                                return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center">Resumo</h2>
-                            }
-                            return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center text-[rgb(0,0,0,54%)]">Resumo</h2>
-                            }
-                        )()}
-                        
-                    </Link>    
-                    <Link href={`/home/materiais/${id}/${idMaterial}/Flashcards`} className="flex items-center">
-                        {(() => {
-                            if (pathname == `/home/materiais/${id}/${idMaterial}/Flashcards`) {
-                                return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center">Flashcards</h2>
-                            }
-                            return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center text-[rgb(0,0,0,54%)]">Flashcards</h2>
-                            }
-                        )()}
-                        
-                    </Link>     
-                    <Link href={`/home/materiais/${id}/${idMaterial}/Quizzes`} className="flex items-center">
-                        {(() => {
-                            if (pathname == `/home/materiais/${id}/${idMaterial}/Quizzes`) {
-                                return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center">Quizzes</h2>
-                            }
-                            return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center text-[rgb(0,0,0,54%)]">Quizzes</h2>
-                            }
-                        )()}
-                    </Link>     
+            <div className="flex w-full h-full relative overflow-hidden ">
+                <div className="flex flex-col w-full h-full relative overflow-hidden">
+                    <div className="flex gap-6">
+                        <Link href={`/home/materiais/${id}/${idMaterial}/Material`} className="flex items-center">
+                            {(() => {
+                                if (pathname == `/home/materiais/${id}/${idMaterial}/Material`) {
+                                    return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center">Material</h2>
+                                }
+                                return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center text-[rgb(0,0,0,54%)]">Material</h2>
+                                }
+                            )()}
+                            
+                        </Link>    
+                        <Link href={`/home/materiais/${id}/${idMaterial}/Resumo`} className="flex items-center">
+                            {(() => {
+                                if (pathname == `/home/materiais/${id}/${idMaterial}/Resumo`) {
+                                    return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center">Resumo</h2>
+                                }
+                                return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center text-[rgb(0,0,0,54%)]">Resumo</h2>
+                                }
+                            )()}
+                            
+                        </Link>    
+                        <Link href={`/home/materiais/${id}/${idMaterial}/Flashcards`} className="flex items-center">
+                            {(() => {
+                                if (pathname == `/home/materiais/${id}/${idMaterial}/Flashcards`) {
+                                    return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center">Flashcards</h2>
+                                }
+                                return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center text-[rgb(0,0,0,54%)]">Flashcards</h2>
+                                }
+                            )()}
+                            
+                        </Link>     
+                        <Link href={`/home/materiais/${id}/${idMaterial}/Quizzes`} className="flex items-center">
+                            {(() => {
+                                if (pathname == `/home/materiais/${id}/${idMaterial}/Quizzes`) {
+                                    return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center">Quizzes</h2>
+                                }
+                                return <h2 className="text-[25px] font-medium cursor-pointer relative flex justify-center text-[rgb(0,0,0,54%)]">Quizzes</h2>
+                                }
+                            )()}
+                        </Link>     
+                    </div>
+                    <div className={` ${pathname === `/home/materiais/${id}/${idMaterial}/Resumo` ? "ml-[106px] w-[78px]" : pathname === `/home/materiais/${id}/${idMaterial}/Flashcards` ? "ml-[209px] w-[104px]" : pathname === `/home/materiais/${id}/${idMaterial}/Quizzes` ? "ml-[336px] w-[75px]" : pathname === `/home/materiais/${id}/${idMaterial}/Material` ? "w-[83px]" : "" } transition-all ease-in-out duration-300 bg-[#A39CEC] h-[3px] rounded-full `}></div>
                 </div>
-                <div className={` ${pathname === `/home/materiais/${id}/${idMaterial}/Resumo` ? "ml-[106px] w-[78px]" : pathname === `/home/materiais/${id}/${idMaterial}/Flashcards` ? "ml-[209px] w-[104px]" : pathname === `/home/materiais/${id}/${idMaterial}/Quizzes` ? "ml-[336px] w-[75px]" : pathname === `/home/materiais/${id}/${idMaterial}/Material` ? "w-[83px]" : "" } transition-all ease-in-out duration-300 bg-[#A39CEC] h-[3px] rounded-full `}></div>
+
+                <Link href={`/home/materiais/${id}`}>
+                    <motion.button 
+                    whileTap={{ scale:0.98 }}
+                    whileHover={{ scale:1.02 }}
+                    className="flex bg-[#A39CEC] justify-center items-center text-white h-fit text-[20px] py-1 px-4 rounded-full">
+                        <Reply/> Voltar
+                    </motion.button>
+                </Link>
+            
             </div>
         </>
     )
