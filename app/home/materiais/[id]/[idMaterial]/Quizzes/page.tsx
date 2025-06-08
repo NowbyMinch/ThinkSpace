6656f2ea5af43191c10d8bf9a9e5c54a73746b0e
Monteiro <guilhermemr2011j3@gmail.com>
2025-06-07 23:09:00 -0300

    update

--- app/home/materiais/[id]/[idMaterial]/Quizzes/page.tsx
+++ app/home/materiais/[id]/[idMaterial]/Quizzes/page.tsx
@@ -16,13 +16,14 @@ const questoes = [
 
 export default function MaterialClient() {
     const [questaoIndex, setQuestaoIndex] = useState(0);
+    let barlength = (questaoIndex / (questoes.length - 1) ) * 100;
 
     return( 
         <>  
             <div className="bg-white rounded-[35px] h-[100%] overflow-hidden flex flex-col items-center  shadow-md border border-[#00000031]">
                 <div className="w-[1200px] max-w-[90%] h-[850px] max-h-[92%] mt-[35px] mb-[35px] relative flex justify-center items-center">
                     <div className="w-full h-[10px] rounded-full bg-[rgba(16,19,46,0.14)] absolute top-0">
-                        <div className="w-[20%] h-full rounded-full bg-[rgba(30,35,81,0.75)]"></div>
+                        <div style={{ width: `${barlength}%` }} className="transition-all ease-in-out duration-300 h-full rounded-full bg-[rgba(30,35,81,0.75)]"></div>
                     </div>
 
                     <div className="w-full h-[75%] bg-[#F7F7FF] rounded-[25px] border-[2px] shadow-md border-[rgba(60,49,91,0.24)] flex justify-center items-center">
@@ -33,14 +34,14 @@ export default function MaterialClient() {
                             <div className="w-[85%] h-[80%] flex flex-col gap-[5%] justify-center items-center ">
                                 <h1 className="text-[35px] text-center line-clamp-2 break-words ">{questoes[questaoIndex]?.questao}</h1>
                                 <div className="w-full  flex flex-col gap-[5%]">
-                                    <div className="flex max-w-[100%] overflow-hidden gap-[5%]">
+                                    <div className="flex max-w-[100%]  gap-[5%]">
                                         <motion.button 
                                         whileTap={{ scale: 0.99 }}
                                         whileHover={{ scale: 1.01, backgroundColor: "#A39CEC", color: "#FFFFFF"}}
                                         transition={{ 
                                             backgroundColor: { duration: 0.15, ease: "easeInOut" }
                                         }}  
-
+                                        onClick={() => {if (questaoIndex !== questoes.length - 1){ setQuestaoIndex(questaoIndex + 1); console.log(questaoIndex)} else {return} }}
                                         className=" bg-white border-[2px] w-[50%] max-w-[50%] h-[100px] max-h-[95%] rounded-[20px] border-[#726BB6] shadow-md flex justify-center items-center text-[30px] font-medium ">
                                             <span className="line-clamp-1 break-words">{questoes[questaoIndex]?.resposta1}</span>
                                         </motion.button>
@@ -51,20 +52,20 @@ export default function MaterialClient() {
                                         transition={{ 
                                             backgroundColor: { duration: 0.15, ease: "easeInOut" }
                                         }}  
-
+                                        onClick={() => {if (questaoIndex !== questoes.length - 1){ setQuestaoIndex(questaoIndex + 1); console.log(questaoIndex)} else {return} }}
                                         className=" bg-white border-[2px] w-[50%] max-w-[50%]  h-[100px] max-h-[95%] rounded-[20px] border-[#726BB6] shadow-md flex justify-center items-center text-[30px] font-medium">
                                             <span className="line-clamp-1 break-words">{questoes[questaoIndex]?.resposta2}</span>
                                         </motion.button>
                                     </div>
 
-                                    <div className="flex max-w-[100%] overflow-hidden gap-[5%]">
+                                    <div className="flex max-w-[100%]  gap-[5%]">
                                         <motion.button 
                                         whileTap={{ scale: 0.99 }}
                                         whileHover={{ scale: 1.01, backgroundColor: "#A39CEC", color: "#FFFFFF"}}
                                         transition={{ 
                                             backgroundColor: { duration: 0.15, ease: "easeInOut" }
                                         }}  
-
+                                        onClick={() => {if (questaoIndex !== questoes.length - 1){ setQuestaoIndex(questaoIndex + 1); console.log(questaoIndex)} else {return} }}
                                         className=" bg-white border-[2px] w-[50%] max-w-[50%] h-[100px] max-h-[95%] rounded-[20px] border-[#726BB6] shadow-md flex justify-center items-center text-[30px] font-medium ">
                                             <span className="line-clamp-1 break-words">{questoes[questaoIndex]?.resposta3}</span>
                                         </motion.button>
@@ -75,7 +76,7 @@ export default function MaterialClient() {
                                         transition={{ 
                                             backgroundColor: { duration: 0.15, ease: "easeInOut" }
                                         }}  
-
+                                        onClick={() => {if (questaoIndex !== questoes.length - 1){ setQuestaoIndex(questaoIndex + 1); console.log(questaoIndex)} else {return} }}
                                         className=" bg-white border-[2px] w-[50%] max-w-[50%]  h-[100px] max-h-[95%] rounded-[20px] border-[#726BB6] shadow-md flex justify-center items-center text-[30px] font-medium">
                                             <span className="line-clamp-1 break-words">{questoes[questaoIndex]?.resposta4}</span>
                                         </motion.button>
@@ -98,7 +99,6 @@ export default function MaterialClient() {
                             whileTap={{ scale: 0.97}}
                             transition={{ duration: 0.3, ease: "easeInOut"}}
                             onClick={() => {if (questaoIndex !== questoes.length - 1){ setQuestaoIndex(questaoIndex + 1); console.log(questaoIndex)} else {return} }}
-
                             className="bg-white rounded-full p-5 border-[1px] border-[rgba(0,0,0,0.3)] cursor-pointer absolute bottom-0 right-0">
                                 <ArrowRight className=""/>
                             </motion.div>
