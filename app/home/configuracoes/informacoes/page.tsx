76a585b523d28c850ed5f5dd275f967d5fc5056e
Maria Eduarda Machado <121265696+mari-machado@users.noreply.github.com>
2025-06-16 11:34:55 -0300

    Update page.tsx

--- app/home/configuracoes/informacoes/page.tsx
+++ app/home/configuracoes/informacoes/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+// import { set } from 'date-fns';
 import { motion } from 'framer-motion';
+// import { UserSquareIcon } from 'lucide-react';
 import { useEffect, useState } from 'react';
 
 type UserData = {
@@ -10,8 +12,35 @@ type UserData = {
   // add other properties if needed
 };
 
+type UsuarioData = {
+  primeiroNome?: string;
+  cargo?: string;
+  foto?: string;
+  areaDeInteresse?: string;
+  atualizadoEm?: string;
+  codigoExpiracao?: string;
+  codigoVerificado?: string;
+  criadoEm?: string;
+  dataNascimento?: string;
+  email?: string;
+  emailVerificado?: boolean;
+  escolaridade?: string;
+  funca?: string; 
+  id?: string;
+  instituicaoId?: string;
+  nomeCompleto?: string;
+  objetivoNaPlataforma?: string;
+  senha?: string;
+  sobrenome?: string;
+  ultimoLogin?: string;
+};
+
 export default function Informações() {
-  const [ user, setUser ] = useState<UserData>({})
+  const [ user, setUser ] = useState<UserData>({});
+  const [usuario, setUsuario] = useState<UsuarioData>({});
+
+  
+  const [ instituicao, setInstituicao ] = useState<string>("");
 
   useEffect(() => {
     const user = async () => {
@@ -37,15 +66,52 @@ export default function Informações() {
         });
         
         const data = await res.json();
-        console.log(data)
+        setUsuario(data.usuario);
       } catch (err) {
         // setMessage("Erro ao carregar saudação.");
         console.error(err);
       }
-    }; 
-    e();      
+    }; e();
 
+    const instituicao = async () => {
+      try{
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/instituicao/nome`, {
+          method: 'GET',
+          credentials: 'include',
+        });
+        
+        const data = await res.json();
+        console.log(data.nome);
+        setInstituicao(data.nome);
+        // setInstituicao(data.instituicao.nome);
+
+      } catch (err) {
+        // setMessage("Erro ao carregar saudação.");
+        console.error(err);
+      }
+    }; instituicao();      
   }, []);
+  
+  // const [ configuracoes, setConfiguracoes ] = useState({ primeiroNome: usuario.primeiroNome, sobrenome: usuario.sobrenome, dataNascimento: usuario.dataNascimento?.split('T')[0], instituicaoId: usuario.instituicaoId, nomeCompleto: usuario.nomeCompleto, escolaridade: usuario.escolaridade, funcao: user.cargo });
+
+  // const editarConfiguracoes = async (e) => {
+  //   e.preventDefault();
+  //   console.log(configuracoes);
+  //   try {
+  //       const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/configuracoes`, {
+  //         method: "PATCH",
+  //         credentials: "include",
+  //         headers: { 'Content-Type': 'application/json' },
+  //         body: JSON.stringify(configuracoes),
+  //       });
+  
+  //       const result = await res.json();
+  //       console.log(result.message); // Matéria excluída com sucesso.
+        
+  //     } catch (error) {
+  //       console.error("Erro ao editar a senha:");
+  //     }
+  // };
 
   return (
     <>
@@ -54,7 +120,8 @@ export default function Informações() {
           <h1 className="text-[30px] font-medium">Primeiro nome</h1>
           <input
             type="text"
-            defaultValue=""
+            defaultValue={usuario.primeiroNome}
+            // onChange={(e) => setConfiguracoes({ ...configuracoes, primeiroNome: e.target.value })}
             className=" rounded-[20px] border-[2px] border-[#0d0f224e] pl-2 w-[60%] text-[25px] h-[60px] outline-[#9767F8]"
           ></input>
         </div>
@@ -62,7 +129,8 @@ export default function Informações() {
           <h1 className="text-[30px] font-medium">Sobrenome</h1>
           <input
             type="text"
-            defaultValue=""
+            defaultValue={usuario.sobrenome }
+            // onChange={(e) => setConfiguracoes({ ...configuracoes, sobrenome: e.target.value })}
             className=" rounded-[20px] border-[2px] border-[#0d0f224e] pl-2 w-[60%] text-[25px] h-[60px] outline-[#9767F8]"
           ></input>
         </div>
@@ -71,7 +139,8 @@ export default function Informações() {
           <h1 className="text-[30px] font-medium">Data de Nascimento</h1>
           <input
             type="text"
-            defaultValue=""
+            defaultValue={usuario?.dataNascimento?.split('T')[0] ?? ""}
+            // onChange={(e) => setConfiguracoes({ ...configuracoes, dataNascimento: e.target.value })}
             className=" rounded-[20px] border-[2px] border-[#0d0f224e] pl-2 w-[60%] text-[25px] h-[60px] outline-[#9767F8]"
           ></input>
         </div>
@@ -80,7 +149,8 @@ export default function Informações() {
           <h1 className="text-[30px] font-medium">Instituição</h1>
           <input
             type="text"
-            defaultValue=""
+            defaultValue={instituicao}
+            // onChange={(e) => setConfiguracoes({ ...configuracoes, instituicaoId: e.target.value })}
             className=" rounded-[20px] border-[2px] border-[#0d0f224e] pl-2 w-[60%] text-[25px] h-[60px] outline-[#9767F8]"
           ></input>
         </div>
@@ -91,7 +161,8 @@ export default function Informações() {
           </h1>
           <input
             type="text"
-            defaultValue=""
+            defaultValue={usuario?.escolaridade ?? ""}
+            // onChange={(e) => setConfiguracoes({ ...configuracoes, funcao: e.target.value })}
             className=" rounded-[20px] border-[2px] border-[#0d0f224e] pl-2 w-[60%] text-[25px] h-[60px] outline-[#9767F8]"
           ></input>
         </div>
@@ -101,6 +172,7 @@ export default function Informações() {
         whileTap={{ scale: 0.99 }}
         type='submit'
         id="editar_conta"
+        // onClick={editarConfiguracoes}
         className="mt-3 ml-1 w-[200px] h-[60px] rounded-[30px] text-[25px] font-medium border border-[#1E2351]"
         >
           Salvar
